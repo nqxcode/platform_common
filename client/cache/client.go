37976587c37c66ecb48769de676ad89e697613ca
Andrey Pokoev <avpokoyev@mts.ru>
2024-08-24 00:14:32 +0300

    week_4 WIP

--- client/cache/client.go
+++ client/cache/client.go
@@ -23,12 +23,15 @@ type RedisClient interface {
 // KeyComparator comparator function type
 type KeyComparator func(a, b string) bool
 
+// ScanOptions scan options
 type ScanOptions struct {
 	KeyComparator *KeyComparator
 }
 
+// ScanOption scan option
 type ScanOption func(*ScanOptions)
 
+// WithKeyComparator key comparator function
 func WithKeyComparator(keyComparator KeyComparator) ScanOption {
 	return func(options *ScanOptions) {
 		options.KeyComparator = &keyComparator

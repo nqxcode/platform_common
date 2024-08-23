cc6d194ada1b1b19b6c532f2e7999d95975291e8
Andrey Pokoev <avpokoyev@mts.ru>
2024-08-23 13:45:23 +0300

    week_4 WIP

--- client/cache/config.go
+++ client/cache/config.go
@@ -1,8 +1,13 @@
 package cache
 
+import "time"
+
 // RedisConfig redis config
 type RedisConfig interface {
 	Address() string
+	ConnectionTimeout() time.Duration
+	MaxIdle() int
+	IdleTimeout() time.Duration
 	Password() string
 	DB() int
 }
---
+++ client/cache/redis/client.go
@@ -0,0 +1,185 @@
+package redis
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/nqxcode/platform_common/client/cache"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+var _ cache.RedisClient = (*client)(nil)
+
+type handler func(ctx context.Context, conn redis.Conn) error
+
+type client struct {
+	pool   *redis.Pool
+	config cache.RedisConfig
+}
+
+func NewClient(pool *redis.Pool, config cache.RedisConfig) *client {
+	return &client{
+		pool:   pool,
+		config: config,
+	}
+}
+
+// HashSet set values for key
+func (c *client) HashSet(ctx context.Context, key string, values interface{}) error {
+	err := c.execute(ctx, func(ctx context.Context, conn redis.Conn) error {
+		_, err := conn.Do("HSET", redis.Args{key}.AddFlat(values)...)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Set value by key
+func (c *client) Set(ctx context.Context, key string, value interface{}) error {
+	err := c.execute(ctx, func(ctx context.Context, conn redis.Conn) error {
+		_, err := conn.Do("SET", redis.Args{key}.Add(value)...)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// HGetAll get fields by key
+func (c *client) HGetAll(ctx context.Context, key string) ([]interface{}, error) {
+	var values []interface{}
+	err := c.execute(ctx, func(ctx context.Context, conn redis.Conn) error {
+		var errEx error
+		values, errEx = redis.Values(conn.Do("HGETALL", key))
+		if errEx != nil {
+			return errEx
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return values, nil
+}
+
+// Get by key
+func (c *client) Get(ctx context.Context, key string) (interface{}, error) {
+	var value interface{}
+	err := c.execute(ctx, func(ctx context.Context, conn redis.Conn) error {
+		var errEx error
+		value, errEx = conn.Do("GET", key)
+		if errEx != nil {
+			return errEx
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return value, nil
+}
+
+// Expire value for key
+func (c *client) Expire(ctx context.Context, key string, expiration time.Duration) error {
+	err := c.execute(ctx, func(ctx context.Context, conn redis.Conn) error {
+		_, err := conn.Do("EXPIRE", key, int(expiration.Seconds()))
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Ping ping redis
+func (c *client) Ping(ctx context.Context) error {
+	err := c.execute(ctx, func(ctx context.Context, conn redis.Conn) error {
+		_, err := conn.Do("PING")
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// FlushDB flush redis db
+func (c *client) FlushDB(ctx context.Context) error {
+	err := c.execute(ctx, func(ctx context.Context, conn redis.Conn) error {
+		_, err := conn.Do("FLUSHDB")
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *client) execute(ctx context.Context, handler handler) error {
+	conn, err := c.getConnect(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err = conn.Close()
+		if err != nil {
+			log.Printf("failed to close redis connection: %v\n", err)
+		}
+	}()
+
+	err = handler(ctx, conn)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *client) getConnect(ctx context.Context) (redis.Conn, error) {
+	getConnTimeoutCtx, cancel := context.WithTimeout(ctx, c.config.ConnectionTimeout())
+	defer cancel()
+
+	conn, err := c.pool.GetContext(getConnTimeoutCtx)
+	if err != nil {
+		log.Printf("failed to get redis connection: %v\n", err)
+
+		_ = conn.Close()
+		return nil, err
+	}
+
+	return conn, nil
+}

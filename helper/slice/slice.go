f166c0c7303e6c8e4c34299862e67a93b6e2d854
Andrey Pokoev <avpokoyev@mts.ru>
2024-08-24 02:31:03 +0300

    week_4 WIP

--- helper/slice/slice.go
+++ helper/slice/slice.go
@@ -1,9 +1,19 @@
 package slice
 
+import "strconv"
+
 func ToAnySlice[T any](v []T) []any {
 	result := make([]any, len(v))
 	for i := range v {
 		result[i] = v[i]
+	}
+	return result
+}
+
+func ToStringSliceFromIntSlice(v []int64) []string {
+	result := make([]string, len(v))
+	for i := range v {
+		result[i] = strconv.FormatInt(v[i], 10)
 	}
 	return result
 }
